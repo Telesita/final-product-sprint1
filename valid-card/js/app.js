ef971ef885251399cc8fe7749c05526b43d08300
Telesita <ana.garcia8523@gmail.com>
2017-10-31 15:29:12 -0600

    Version 2 from Valid Card, now the program is ready!

--- valid-card/js/app.js
+++ valid-card/js/app.js
@@ -2,49 +2,54 @@ var cardNumber = prompt('Wich is your number card?');
 
 function isValidCard (number){
   
-  var arrayNumber = cardNumber;
-  var separateArray = arrayNumber.split('').reverse();
-  var newArrayNumber = [];
-  var arrayForEvenNumbers=[];
-  var arrayForOddsNumbers = [];
-  var arrayWithAllNumbers = [];
-  var sum = 0;
+  var arrayNumber = cardNumber,
+      separateArray = arrayNumber.split('').reverse(),
+      newArrayNumber = [],
+      arrayForEvenNumbers = [],
+      joinArrayNumbers = [],
+      fromStringToEvenNumbers = [],
+      sumEvenNumbers = 0,
+      arrayForOddsNumbers = [],
+      arrayWithAllNumbers = [],
+      totalSum = 0;
   
   for (var i=0;i<separateArray.length;i++){
-    newArrayNumber.push(separateArray[i]);
-    
-  }
+    newArrayNumber.push(separateArray[i]*1);
+  } 
   
   for (var j=1;j<newArrayNumber.length;j+=2){
     arrayForEvenNumbers.push(newArrayNumber[j]*2);
-    
-    
+  } 
+  
+  joinArrayNumbers = (arrayForEvenNumbers.join('')).split('');
+  
+  for (var k=0;k<joinArrayNumbers.length;k++){
+    fromStringToEvenNumbers.push(joinArrayNumbers[k]*1);
   }
   
-  for(var k=0;k<newArrayNumber.length;k+=2){
-    arrayForOddsNumbers.push(newArrayNumber[k]*1);
-    
-  }  
-  
-  
-  arrayWithAllNumbers = arrayForOddsNumbers.concat(arrayForEvenNumbers);
-  
-  for(l=0;l<arrayWithAllNumbers.length;l++){
-    sum = sum += arrayWithAllNumbers[l];
+  for(l=0;l<fromStringToEvenNumbers.length;l++){
+    sumEvenNumbers = sumEvenNumbers += fromStringToEvenNumbers[l];
   }
   
-  var result = sum;
+  for(var m=0;m<newArrayNumber.length;m+=2){
+    arrayForOddsNumbers.push(newArrayNumber[m]*1);
+  }
   
-  //console.log(sum%10);
+  arrayWithAllNumbers = arrayForOddsNumbers.concat(sumEvenNumbers);
+  
+  for(p=0;p<arrayWithAllNumbers.length;p++){
+    totalSum = totalSum += arrayWithAllNumbers[p];
+  }
+  
+  var result = (totalSum%10);
   
   if (result%10 === 0){
-    return console.log('Your card is Valid!')
+    return console.log('Your card is Valid!');
   }else{
-    return console.log('Your card is Not Valid!')
+    return console.log('Your card is Not Valid!');
   }
   
-  
 }
-
+  
 
 isValidCard (cardNumber);
